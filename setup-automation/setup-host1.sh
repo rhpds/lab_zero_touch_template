18428006e21d77c21a89545d8f0ca611134eff2c
Wilson Harris <wharris@redhat.com>
2025-08-19 21:58:37 -0400

    Remove setup scripts, single terminal

--- setup-automation/setup-host1.sh
+++ setup-automation/setup-host1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-USER=rhel
+# USER=rhel
 
-echo "Adding wheel" > /root/post-run.log
-usermod -aG wheel rhel
+# echo "Adding wheel" > /root/post-run.log
+# usermod -aG wheel rhel
 
-echo "Setup vm control01" > /tmp/progress.log
+# echo "Setup vm control01" > /tmp/progress.log
 
-chmod 666 /tmp/progress.log
+# chmod 666 /tmp/progress.log
 
-#dnf install -y nc
+# #dnf install -y nc
 
